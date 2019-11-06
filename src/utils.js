625265f0eb25a106e2130c6ea9180bc35977236a
Kelvin Esegbona <kevoesegbona@gmail.com>
2019-11-06 08:01:47 +0100

    Added validation for percentage updated readme fix memor leak issues and bugs during mounting

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,24 @@
-import { stringify } from "querystring";
+export class delay {
+  constructor(time) {
+    this.time = time;
+    this.timeout = null;
+    this.close = null;
+  }
 
-export const delay = time =>
-  new Promise(resolve => {
-    setTimeout(() => {
-      resolve();
-    }, time);
-  });
+  getPromise() {
+    return new Promise((resolve, reject) => {
+      this.close = reject;
+      this.timeout = setTimeout(() => {
+        resolve();
+      }, this.time);
+    });
+  }
+  cancel() {
+    this.timeout && clearTimeout(this.timeout);
+    this.close && this.close();
+    return { isCanceled: true };
+  }
+}
 
 export const contentInView = element => {
   const scroll = window.scrollY || window.pageYOffset;
@@ -14,12 +27,12 @@ export const contentInView = element => {
 
   const viewport = {
     top: scroll,
-    bottom: scroll + window.innerHeight
+    bottom: scroll + window.innerHeight,
   };
 
   const elementPosition = {
     top: elementTopPosition,
-    bottom: elementTopPosition + elementPositionProps.height
+    bottom: elementTopPosition + elementPositionProps.height,
   };
   return (
     (elementPosition.bottom >= viewport.top &&
@@ -29,16 +42,25 @@ export const contentInView = element => {
   );
 };
 
+export const sanitizePercentage = percent => {
+  let percentage = Number(percent);
+  if (percentage < 0) percentage = 0;
+  if (percentage > 100) percentage = 100;
+
+  return percentage;
+};
+
 export const getPercentLevel = percentage => {
-  let level = 252 - percentage * 2.52;
+  let level = 252 - sanitizePercentage(percentage) * 2.52;
   return `${level}%`;
 };
 
-export const getColorType = (percent, defColor) => {
-  const poor = (defColor && defColor.poor) || "#F32013";
-  const fair = (defColor && defColor.fair) || "#ff6700";
-  const good = (defColor && defColor.good) || "rgb(255, 217, 0)";
-  const excellent = (defColor && defColor.excellent) || "#48AE2C";
+export const getColorType = (percentage, defColor) => {
+  const percent = sanitizePercentage(percentage);
+  const poor = (defColor && defColor.poor) || '#F32013';
+  const fair = (defColor && defColor.fair) || '#ff6700';
+  const good = (defColor && defColor.good) || 'rgb(255, 217, 0)';
+  const excellent = (defColor && defColor.excellent) || '#48AE2C';
 
   const res =
     (percent < 25 && poor) ||
@@ -49,8 +71,8 @@ export const getColorType = (percent, defColor) => {
 };
 
 export const getNumber = str => {
-  const arr = str.split("");
-  let res = "";
+  const arr = str.split('');
+  let res = '';
   for (let i = 0; i < arr.length; i++) {
     if (isNaN(Number(arr[i]))) break;
     res += arr[i];
@@ -61,13 +83,13 @@ export const getNumber = str => {
 export const getRadius = raw => {
   const width = Number(getNumber(raw));
   if (width < 20 || isNaN(width)) return 250;
-  const radius = raw.trim().slice(-1) === "%" ? width / 2 + "%" : width / 2;
+  const radius = raw.trim().slice(-1) === '%' ? width / 2 + '%' : width / 2;
   return radius;
 };
 
 export const propTypeValidation = {
   width: (props, propName) => {
-    if (!props[propName] || typeof props[propName] != "string")
+    if (!props[propName] || typeof props[propName] != 'string')
       return new Error(
         `Invalid ${propName} supplied to the progress bar component. Please provide a width!`
       );
@@ -75,7 +97,7 @@ export const propTypeValidation = {
   percentage: (props, propName) => {
     if (
       !props[propName] ||
-      (props[propName] && typeof props[propName] != "string")
+      (props[propName] && typeof props[propName] != 'string')
     )
       return new Error(
         `Invalid ${propName} supplied to progress bar component. Please provide a percentage value!`
@@ -83,9 +105,9 @@ export const propTypeValidation = {
   },
   height: (props, propName) => {
     if (
-      props["rect"] &&
+      props['rect'] &&
       (!props[propName] ||
-        (props[propName] && typeof props[propName] != "string"))
+        (props[propName] && typeof props[propName] != 'string'))
     )
       return new Error(
         `Invalid ${propName} supplied to progress bar component. Please provide a height!`
@@ -93,55 +115,61 @@ export const propTypeValidation = {
   },
   rectPadding: (props, propName) => {
     if (
-      props["rect"] &&
-      (!props["height"] || typeof props["height"] != "string")
+      props['rect'] &&
+      (!props['height'] || typeof props['height'] != 'string')
     )
       return new Error(
         `Invalid information supplied to progress bar component. Please provide a height before you provide this ${propName}!`
       );
   },
   trackBorderColor: (props, propName) => {
-    if (props[propName] && typeof props[propName] != "string")
+    if (props[propName] && typeof props[propName] != 'string')
       return new Error(
         `Invalid ${propName} supplied to progress bar component!`
       );
   },
   fontColor: (props, propName) => {
-    if (props[propName] && typeof props[propName] != "string")
+    if (props[propName] && typeof props[propName] != 'string')
       return new Error(
         `Invalid ${propName} supplied to progress bar component!`
       );
   },
   rectBorderRadius: (props, propName) => {
-    if (props[propName] && typeof props[propName] != "string")
+    if (props[propName] && typeof props[propName] != 'string')
       return new Error(
         `Invalid ${propName} supplied to progress bar component!`
       );
   },
   trackPathColor: (props, propName) => {
-    if (props[propName] && typeof props[propName] != "string")
+    if (props[propName] && typeof props[propName] != 'string')
       return new Error(
         `Invalid ${propName} supplied to progress bar component!`
       );
   },
   hollowBackgroundColor: (props, propName) => {
-    if (props[propName] && typeof props[propName] != "string")
+    if (props[propName] && typeof props[propName] != 'string')
+      return new Error(
+        `Invalid ${propName} supplied to progress bar component!`
+      );
+  },
+  scrollArea: (props, propName) => {
+    if (props[propName] && typeof props[propName] != 'object')
       return new Error(
         `Invalid ${propName} supplied to progress bar component!`
       );
   },
   defColor: (props, propName) => {
-    if (props[propName] && typeof props[propName] == "object") {
+    if (props[propName] && typeof props[propName] == 'object') {
       const arr = Object.keys(props[propName]);
       for (let i = 0; i < arr.length; i++) {
-        if (typeof props[propName][arr[i]] != "string")
+        if (typeof props[propName][arr[i]] != 'string')
           return new Error(
             `Invalid  property Key ${arr[i]} for ${propName} supplied to progress bar component!`
           );
       }
-    } else if (props[propName] && typeof props[propName] !== "object")
+    } else if (props[propName] && typeof props[propName] !== 'object')
       return new Error(
         `Invalid ${propName} supplied to progress bar component!`
       );
-  }
+  },
 };
